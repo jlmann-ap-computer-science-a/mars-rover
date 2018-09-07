d6bd6c9134de334a1742093ee856b07ef91b3d0e
Jon Cooper <joncoop@users.noreply.github.com>
2018-09-07 12:40:29 -0400

    Add files via upload

--- Rover.java
+++ Rover.java
@@ -1,78 +1,91 @@
 
 /**
  * Write a description of class Rover here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Rover
 {
-    // fields
-    String name;
-    int x;
-    int y;
-    int dir; // 0=North, 1=East, 2=South, 3=West
-    
-    
-    // constructor(s)
+    // instance variables
+    private int x;
+    private int y;
+    private int dir;
+    private String name;
+    private boolean isAlive;
+
     public Rover(String name)
     {
-        this.name = name;
         this.x = 0;
         this.y = 0;
         this.dir = 0;
+        this.name = name;
+        this.isAlive = true;
     }
     
-    
-    // methods - stuff the Rover can do
-    public void move()
+    public Rover(String name, int x, int y, int dir)
     {
-        if (dir == 0)
-        {
-            y += 1;
-        }
-        else if (dir == 1)
-        {
-            x += 1;
-        }
-        else if (dir == 2)
-        {
-            y -= 1;
-        }
-        else 
-        {
-            x -= 1;
-        }
-        
-        System.out.println(name + " moved in direction " + dir);
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.name = name;
+        this.isAlive = true;
     }
-    
-    public void rotateLeft() 
-    {
-        dir -= 1;
-        
-        if (dir < 0)
-        {
-            dir = 3;
-        }
-        
-        System.out.println(name + " turned to the left");        
-    }
-    
+
     public void rotateRight()
     {
-        dir += 1;
+        dir = dir + 1;
         
-        if (dir == 4)
-        {
+        if (dir == 4) {
             dir = 0;
         }
         
-        System.out.println(name + " turned to the right");        
+        System.out.println(this.name + " turned to the right");
     }
-
-    public String toString() 
+    
+    public void rotateLeft()
     {
-        return "Rover[name=" + name + ", x=" + x + ", y=" + y + ", dir=" + dir + "]";
+        dir = dir - 1;
+        
+        if (dir == -1) {
+            dir = 3;
+        }
+        
+        System.out.println(this.name + " turned to the left");
+    }
+    
+    public void move() 
+    {
+        if (isAlive) {
+            if (dir == 0) {
+                y = y + 1;
+            }
+            else if (dir == 1) {
+                x = x + 1;
+            }
+            else if (dir == 2) {
+                y = y - 1;
+            }
+            else {
+                x = x - 1;
+            }
+            
+            System.out.println(this.name + " moved forward");
+        }
+        else {
+            System.out.println(this.name + " can't move. It's ded.");
+        }
+    }
+    
+    public void kill(Rover other) {
+        System.out.println(this.name + " shoots " + other.name + " with space lasers.");
+        System.out.println(other.name + " goes 'beep beep aaaaaaakkkkkk!' and dies");
+        
+        other.isAlive = false;
+    }
+    
+    public String toString() {
+        return "Rover[name=" + name + ",x=" + x + ",y=" + y + 
+               ",dir=" + dir + ",isAlive=" + isAlive + "]";
     }
 }
